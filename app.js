4a9775fc4b42d45448945e7bb86fed0812c88752
Itor Carlos <itor51carlos@gmail.com>
2024-09-11 21:20:19 -0300

    add create table if table Pessoa doesnt exist

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const {Client} = require("pg");
+const { Client } = require("pg");
 
 const arrayPessoasInsert = [
     {
         documento: "10022479",
-        telefones: ["74999250794","79981396317"],
+        telefones: ["74999250794", "79981396317"],
         primeiro_nome: "Arnaldo",
         sobrenome: "Cesar Coelho",
     },
@@ -19,7 +19,7 @@ const arrayPessoasInsert = [
         primeiro_nome: "Capitão",
         sobrenome: "America",
     }
-]
+];
 
 const conexaoPostgres = new Client({
     host: 'localhost',
@@ -27,43 +27,58 @@ const conexaoPostgres = new Client({
     user: "root",
     password: "root",
     database: "projeto_logico"
-})
+});
 
-async function realizaConexaoPostgres(){
-    try{
+async function realizaConexaoPostgres() {
+    try {
         await conexaoPostgres.connect();
-        console.log("Conexão com o PostgreSQL estabelecida.\n");
-    }
-    catch(error){
+
+        await conexaoPostgres.query(`
+            CREATE TABLE IF NOT EXISTS Pessoa (
+                documento VARCHAR(50) PRIMARY KEY,
+                telefones TEXT[],
+                primeiro_nome VARCHAR(100),
+                sobrenome VARCHAR(100)
+            );
+        `);
+    } catch (error) {
         console.log(`Erro ao realizar conexão com o banco PostgreSQL: ${error}`);
     }
 }
 
-async function fechaConexaoPostgres(){
+async function fechaConexaoPostgres() {
     await conexaoPostgres.end();
     console.log("Conexão com o PostgreSQL encerrada\n");
 }
 
-async function inserirPessoa({documento, telefones, primeiro_nome, sobrenome}){
+async function inserirPessoa({ documento, telefones, primeiro_nome, sobrenome }) {
+    await conexaoPostgres.query('BEGIN');
     const sql_insert = 'INSERT INTO Pessoa (documento, telefones, primeiro_nome, sobrenome) VALUES ($1,$2,$3,$4)';
-    try{
+    try {
         await conexaoPostgres.query(sql_insert, [documento, telefones, primeiro_nome, sobrenome]);
         console.log(`Pessoa -> ${documento}-${telefones}-${primeiro_nome}-${sobrenome} criado com sucesso`);
-    }
-    catch(errorInsert){
+        await conexaoPostgres.query('COMMIT');
+    } catch (errorInsert) {
+        await conexaoPostgres.query('ROLLBACK');
         console.log(`Error ao realizar insert: ${errorInsert}`);
     }
 }
 
-async function getAllPessoas(){
+async function getAllPessoas() {
     const sql_select = 'SELECT * FROM Pessoa;';
     const resultSelect = (await conexaoPostgres.query(sql_select)).rows;
     let contador = 1;
     resultSelect.forEach(pessoa => console.log(`pessoa n° ${contador++} | documento: ${pessoa.documento} | telefones: ${pessoa.telefones} | primeiro_nome: ${pessoa.primeiro_nome} | sobrenome: ${pessoa.sobrenome}\n`));
 }
 
-realizaConexaoPostgres();
+async function main() {
+    await realizaConexaoPostgres();
 
-arrayPessoasInsert.forEach(pessoa => inserirPessoa(pessoa));
+    for (const pessoa of arrayPessoasInsert) {
+        await inserirPessoa(pessoa);
+    }
 
-getAllPessoas();
+    await getAllPessoas();
+}
+
+main();
